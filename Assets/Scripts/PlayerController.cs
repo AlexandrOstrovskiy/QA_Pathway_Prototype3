7cb86da4a6a5b448be5d9ceefb7c1f833b79cdd7
AlexandrOstrovskiy <a.ostrovskiy@rj-games.com>
2021-03-04 01:09:11 +0300

    3.4 Done
    Added particle effects for running foots, death visual and audio effects, jump sound and looped background music

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,14 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRigidbody;
     private Animator playerAnimator;
 
+    private AudioSource playerAudio;
+
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+
+    public AudioClip jumpSound;
+    public AudioClip deathSound;
+
     public float jumpForce = 16.0f;
     public float gravityModifier = 5.0f;
 
@@ -18,17 +26,21 @@ public class PlayerController : MonoBehaviour
     {
         playerRigidbody = GetComponent<Rigidbody>();
         playerAnimator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (!gameOver && OnGroud && Input.GetKeyDown(KeyCode.Space))
         {
             playerRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             OnGroud = false;
 
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 5.0f);
             playerAnimator.SetTrigger("Jump_trig");
         }
     }
@@ -36,11 +48,17 @@ public class PlayerController : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
+        {
             OnGroud = true;
+            dirtParticle.Play();
+        }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             Debug.Log("Game Over!");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(deathSound, 5.0f);
+            explosionParticle.Play();
             playerAnimator.SetBool("Death_b", true);
             playerAnimator.SetInteger("DeathType_int", 1);
         }
